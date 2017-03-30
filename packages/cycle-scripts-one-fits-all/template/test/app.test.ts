6da16b5fae2384db101525dd99a468d878062345
Jan van Brügge <supermanitu@gmail.com>
2017-03-30 14:44:41 +0200

    Clean test

--- packages/cycle-scripts-one-fits-all/template/test/app.test.ts
+++ packages/cycle-scripts-one-fits-all/template/test/app.test.ts
@@ -1,11 +1,11 @@
 import { forall, assert, nat, Options } from 'jsverify';
+import { diagramArbitrary } from 'cyclejs-test-helpers';
 import * as htmlLooksLike from 'html-looks-like';
 const toHtml = require('snabbdom-to-html'); //snabbdom-to-html's typings are broken
 
-import xs, { Stream } from 'xstream';
-import { VNode, makeHTMLDriver, mockDOMSource } from '@cycle/dom';
+import xs from 'xstream';
+import { mockDOMSource } from '@cycle/dom';
 import { mockTimeSource } from '@cycle/time';
-import { diagramArbitrary } from 'cyclejs-test-helpers';
 import onionify from 'cycle-onionify';
 
 import { App } from '../src/app';
@@ -15,41 +15,7 @@ const testOptions : Options = {
     size: 200
 };
 
-function mockStateSource(count : number) : any
-{
-    return {
-        state$: xs.of({ count: count })
-    };
-}
-
 describe('app tests', () => {
-
-    it('should always print same text', (done) => {
-        const staticHTML = count => `
-            <div>
-                <h2>My Awesome Cycle.js app</h2>
-                {{ ... }}
-                <button type="button" class="add">Increase</button>
-                <button type="button" class="subtract">Decrease</button>
-            </div>
-        `;
-
-        const property = forall(nat, (n) => {
-            const Time = mockTimeSource();
-            const DOM = mockDOMSource({});
-            const onion = mockStateSource(n);
-
-            const app = App({ DOM, onion } as any);
-            const html$ = app.DOM.map(toHtml);
-
-            Time.assertEqual(html$, xs.of(n).map(staticHTML), htmlLooksLike);
-            return new Promise((resolve, reject) => Time.run(err => err ? reject(err) : resolve(true)));
-        });
-
-        assert(property, testOptions)
-            .then(val => val ? done(val) : done())
-            .catch(err => done(err));
-    });
 
     const expectedHTML = count => `
         <div>
@@ -58,25 +24,6 @@ describe('app tests', () => {
             {{ ... }}
         </div>
     `;
-
-    it('should always print the correct number', (done) => {
-        const property = forall(nat, (n) => {
-            const Time = mockTimeSource();
-            const DOM = mockDOMSource({});
-            const onion = mockStateSource(n);
-
-            const app = App({ DOM, onion } as any);
-            const html$ = app.DOM.map(toHtml);
-
-            Time.assertEqual(html$, xs.of(n).map(expectedHTML), htmlLooksLike);
-
-            return new Promise((resolve, reject) => Time.run(err => err ? reject(err) : resolve(true)));
-        });
-
-        assert(property, testOptions)
-            .then(val => val ? done(val) : done())
-            .catch(err => done(err));
-    });
 
     it('should interact correctly', (done) => {
         const property = forall(diagramArbitrary, diagramArbitrary, (addDiagram, subtractDiagram) => {
@@ -101,7 +48,7 @@ describe('app tests', () => {
             return new Promise((resolve, reject) => Time.run(err => err ? reject(err) : resolve(true)));
         });
 
-        assert(property, { ...testOptions, tests: testOptions.tests / 2 })
+        assert(property, testOptions)
             .then(val => val ? done(val) : done())
             .catch(err => done(err));
     });
