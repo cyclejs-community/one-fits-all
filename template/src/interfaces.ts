a3ec49a2f90a73ed6db82ce43aa2312e7387c3c8
Steve Lee <steve@opendirective.com>
2017-08-05 10:57:09 +0100

    Update cyclic-router (#14)

--- template/src/interfaces.ts
+++ template/src/interfaces.ts
@@ -2,7 +2,7 @@ import { Stream } from 'xstream';
 import { DOMSource, VNode } from '@cycle/dom';
 import { HTTPSource, RequestOptions } from '@cycle/http';
 import { TimeSource } from '@cycle/time';
-import { RouterSource } from 'cyclic-router';
+import { RouterSource, HistoryAction } from 'cyclic-router';
 
 export type Component = (s: BaseSources) => BaseSinks;
 
@@ -17,7 +17,7 @@ export interface BaseSources {
 export interface BaseSinks {
     DOM?: Stream<VNode>;
     HTTP?: Stream<RequestOptions>;
-    router?: Stream<string | object>;
+    router?: Stream<HistoryAction>;
     storage?: Stream<any>;
     speech?: Stream<string>;
 }
