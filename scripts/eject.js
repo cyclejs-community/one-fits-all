7758d4eb467a0643f71ae65f124234fb54999f83
Fabien JUIF <fabien.juif@gmail.com>
2018-10-21 14:00:16 +0200

    Eject works with yarn and npm (#102)

--- scripts/eject.js
+++ scripts/eject.js
@@ -2,12 +2,10 @@
 
 const fs = require('fs-extra');
 const path = require('path');
-const mkdirp = require('mkdirp');
 const spawn = require('cross-spawn');
 const inquirer = require('inquirer');
 
 const appPath = (...paths) => path.join(process.cwd(), ...paths);
-const usingPnpm = fs.existsSync(appPath('shrinkwrap.yaml'));
 
 const ownDependencyNames = [
     'inquirer',
@@ -17,17 +15,42 @@ const ownDependencyNames = [
     'mkdirp'
 ];
 
+const usingPnpm = fs.existsSync(appPath('shrinkwrap.yaml'));
+const usingYarn = fs.existsSync(appPath('yarn.lock'));
+
+function getDependenciesManagerName() {
+    if (usingPnpm) return 'pnpm';
+    if (usingYarn) return 'yarn';
+    return 'npm';
+}
+
+function getLockFileName() {
+    switch (getDependenciesManagerName()) {
+        case 'yarn':
+            return 'yarn.lock';
+        case 'pnpm':
+            return 'shrinkwrap.yaml';
+        default:
+            return undefined;
+    }
+}
+
+function getPromptMessage() {
+    const baseMessage =
+        'Are you sure you want to eject? This flavor is designed so that you do not have to do so, if I forgot a use case, please open an issue at\n\nhttps://github.com/cyclejs-community/one-fits-all\n\nThis process is NOT reversable, it will remove all traces of the flavor from your project\n';
+
+    if (getDependenciesManagerName() === 'npm') return baseMessage;
+
+    return `${baseMessage}\nYou are using ${getDependenciesManagerName()} which will cause this script to delete your ${getLockFileName()} and node_modules and reinstall afterwards\n`;
+}
+
 inquirer
     .prompt([
         {
             type: 'confirm',
             name: 'confirm',
             default: false,
-            message:
-                'Are you sure you want to eject? This flavor is designed so that you do not have to do so, if I forgot a use case, please open an issue at\n\nhttps://github.com/cyclejs-community/one-fits-all\n\nThis process is NOT reversable, it will remove all traces of the flavor from your project\n' +
-                (!usingPnpm
-                    ? ''
-                    : '\nYou are using pnpm which will cause this script to delete your shrinkwrap.yaml and node_modules and reinstall afterwards\n')
+            message: getPromptMessage()
         }
     ])
     .then(answers => {
@@ -96,9 +119,13 @@ inquirer
             appPath('configs', 'webpack.config.js')
         );
 
-        if (usingPnpm) {
-            fs.removeSync(appPath('shrinkwrap.yaml'));
+        // npm is ok about installing new dependencies
+        // it seems this is not necessary to remove lock file and node_modules
+        if (getDependenciesManagerName() !== 'npm') {
+            fs.removeSync(appPath(getLockFileName()));
             fs.removeSync(appPath('node_modules'));
-            spawn.sync('pnpm', ['install'], { stdio: 'inherit' });
         }
+        spawn.sync(getDependenciesManagerName(), ['install'], {
+            stdio: 'inherit'
+        });
     });
